d7c48dee93cd14ea50885ea724799e4eddb8ca95
mohit <bansal.mohit@jasperindia.com>
2013-04-04 10:43:56 +0530

    changed init.pp included untar of the base-tomcat.tar

--- tomcat/manifests/init.pp
+++ tomcat/manifests/init.pp
@@ -1,47 +1,51 @@
 class tomcat($tomcat_port= 76 , $component=catalog-tomcat,){
-  $server = "/usr/local/${component}"
+	$server = "/usr/local/${component}"
 
 
-  package { "java-1.6.0-openjdk.i386":
-    ensure  => [ ['installed'],['latest']],
-  }
+		package { "java-1.6.0-openjdk.i386":
+			ensure  => [ ['installed'],['latest']],
+		}
 
-  Exec {
-    path => [
-      '/usr/local/bin',
-      '/opt/local/bin',
-      '/usr/bin',
-      '/usr/sbin',
-      '/bin',
-      '/sbin'],
-      logoutput => true,
-  }
+	Exec {
+		path => [
+			'/usr/local/bin',
+			'/opt/local/bin',
+			'/usr/bin',
+			'/usr/sbin',
+			'/bin',
+			'/sbin'],
+			logoutput => true,
+	}
 
-    file { 'directory':
-      name    => "${component}",
-      path    => "${server}",
-    recurse   => true,
-    ensure    => [ ['directory'] , ['present']],
-      before  => [ File[ ['catalina.sh'] , ['server.xml']]],
-      owner   => 'tomcat',
-      group   => 'tomcat',
-      mode    => '0640',
-     source => 'puppet:///modules/tomcat/usr/local/base-tomcat7/',
-  
-    }
+	exec {"untar_folder":
+		user     => 'tomcat',
+			 command   => "tar -xvf /etc/puppetlabs/puppet/modules/tomcat/files/base-tomcat.tar",
+			 logoutput => true,
+			 before    => File['directory'],  
+			 refreshonly => true,
+	}
+	file { 'directory':
+		name    => "${component}",
+			path    => "${server}",
+			recurse   => true,
+			ensure    => [ ['directory'] , ['present']],
+			before  => [ File[ ['catalina.sh'] , ['server.xml']]],
+			owner   => 'tomcat',
+			group   => 'tomcat',
+			mode    => '0640',
+			source => 'puppet:///modules/tomcat/usr/local/base-tomcat7/',
 
-    file { "catalina.sh":
-      ensure  => present,
-      require => File['bin'],
-      path    => "${server}/bin/catalina.sh",
-      content => template('tomcat/catalina.sh'),
-    }
-    file { "server.xml" :
-      ensure  => present, 
-      require => File['conf'],
-      path    => "${server}/conf/server.xml",
-      content => template('tomcat/server.xml'),
-    }
-  }
+	}
 
+	file { "catalina.sh":
+		ensure  => present,
+			path    => "${server}/bin/catalina.sh",
+			content => template('tomcat/catalina.sh'),
+	}
+	file { "server.xml" :
+		ensure  => present, 
+			path    => "${server}/conf/server.xml",
+			content => template('tomcat/server.xml'),
+	}
+}
 
